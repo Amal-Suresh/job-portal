6ad19b0807ec7dd499245adf4701b1cfdf5aab77
Amal-Suresh <amalsureshms2001@gmail.com>
2024-09-06 12:54:44 +0530

    integrated the api to fetch jobs

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,5 +1,25 @@
+import { getJobs } from "@/api/jobs-api"
+import { useSession } from "@clerk/clerk-react"
+import { useEffect } from "react"
 
 const JobListingPage = () => {
+  const { session } = useSession()
+
+  const fetchJobs = async () => {
+
+    const supabaseAccessToken = await session.getToken({
+      template:"supabase"
+    })
+
+    const data = getJobs(supabaseAccessToken)
+
+    console.log(data);
+    
+
+  }
+  useEffect(() => {
+    fetchJobs()
+  }, [])
   return (
     <div>JobListingPage</div>
   )
