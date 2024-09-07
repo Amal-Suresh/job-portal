804260930a4737f34fee86abc802a137711d473c
Amal-Suresh <amalsureshms2001@gmail.com>
2024-09-07 16:54:16 +0530

    job card created

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,27 +1,57 @@
 import { getJobs } from "@/api/jobs-api"
-import { useSession } from "@clerk/clerk-react"
-import { useEffect } from "react"
+import JobCard from "@/components/job-card"
+import useFetch from "@/hooks/use-fetch"
+import { useUser } from "@clerk/clerk-react"
+import { useEffect, useState } from "react"
+import { BarLoader } from "react-spinners"
 
 const JobListingPage = () => {
-  const { session } = useSession()
 
-  const fetchJobs = async () => {
+  const [location, setLocation] = useState("")
+  const [searchQuery, setSearchQuery] = useState("")
+  const [company_id, setCompanyId] = useState("")
+  const { isLoaded } = useUser()
 
-    const supabaseAccessToken = await session.getToken({
-      template:"supabase"
-    })
+  const {
+    fn: fnJobs,
+    data: jobs,
+    loading: loadingJobs
+  } = useFetch(getJobs, { location, company_id, searchQuery })
 
-    const data = getJobs(supabaseAccessToken)
+  console.log(jobs);
 
-    console.log(data);
-    
-
-  }
   useEffect(() => {
-    fetchJobs()
-  }, [])
+    if (isLoaded) fnJobs()
+  }, [isLoaded, location, company_id, searchQuery])
+
+  if (!isLoaded) {
+    return <BarLoader className='mb-4' width={"100%"} color='#36d7b7' />
+  }
+
   return (
-    <div>JobListingPage</div>
+    <div>
+      <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">Latest Jobs</h1>
+
+      {/* Add filters here */}
+      {loadingJobs && (
+        <BarLoader className='mb-4' width={"100%"} color='#36d7b7' />
+      )}
+
+      {loadingJobs === false && (
+        <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {jobs.length ? (
+            jobs.map((job) => (
+              <JobCard key={job.id} job={job} />
+
+            ))
+          ) : (
+            <div>No Jobs Found</div>
+          )
+          }
+        </div>
+      )}
+
+    </div>
   )
 }
 
--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -26,6 +26,7 @@ const Onboarding = () => {
   if (!isLoaded) {
     return <BarLoader className='mb-4' width={"100%"} color='#36d7b7' />
   }
+  
   return (
     <div className='flex flex-col items-center justify-center mt-40'>
       <h2 className='gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter'>
